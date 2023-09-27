644cc36ecff0b16f9e128773043d0674e56e16ab
Shah-ali <ali.shahnwz@live.com>
2023-09-27 15:24:38 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,14 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <ul>
-      <li *ngFor="let todo of todos" >
-        {{ todo.title }} - {{ todo.completed ? 'Completed' : 'Not Completed' }}
-        <button (click)="markCompleted(todo)">Mark Completed</button>
-      </li>
-    </ul>
-  `,
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  todos: { title: string; completed: boolean }[] = [
-    { title: 'Buy groceries', completed: false },
-    { title: 'Finish work report', completed: false },
-    { title: 'Go for a run', completed: true },
+  filterCity: string = '';
+  items: any[] = [
+    { name: 'Item 1', city: 'New York' },
+    { name: 'Item 2', city: 'Los Angeles' },
+    { name: 'Item 3', city: 'Chicago' },
+    // Add more items with city data
   ];
-
-  markCompleted(todo: { title: string; completed: boolean }) {
-    todo.completed = true;
-  }
 }
