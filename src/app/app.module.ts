c2dfd542f4a8c2eeaf644e9b417a480bc69f4183
Shah-ali <ali.shahnwz@live.com>
2023-09-27 16:12:34 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FilterByCityPipe } from './filter-by-city.pipe';
+import { customFilesizePipe } from './custom-filesize.pipe';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, FilterByCityPipe],
+  declarations: [AppComponent, FilterByCityPipe, customFilesizePipe],
   bootstrap: [AppComponent],
-  exports: [FilterByCityPipe],
+  exports: [FilterByCityPipe, customFilesizePipe],
 })
 export class AppModule {}
